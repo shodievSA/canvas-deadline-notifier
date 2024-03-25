68919af440778860f60b8c68c7f122392144e47b
Javohir Nurmatjonov (Manjaro) <javoxirone@gmail.com>
2024-03-25 18:10:33 +0500

    added Deadline info to notifcations

--- utils/bot/notifications.js
+++ utils/bot/notifications.js
@@ -1,24 +1,29 @@
 import { sixHours } from "../../time.js";
+import { bot } from "../../config/integrations.js";
 
-async function scheduleNotifications(assignments, resources, ctx) {
+async function scheduleNotifications(assignments, resources, telegramId) {
 
     for (const assignment of assignments) {
 
         const currentTime = new Date().getTime();
         const deadline = Date.parse(assignment.deadline);
 
-        const delay = deadline - currentTime - sixHours; 
+        const delay = deadline - currentTime - sixHours;
 
         setTimeout(async () => {
 
             const assignmentIDs = (await resources.getAssignments()).map((obj) => obj.id);
 
             if (assignmentIDs.includes(assignment.id)) {
-
-                ctx.sendMessage(
+                const deadline = new Date(assignment.deadline)
+                let date = deadline.toDateString();
+                date = `${date} ${String(deadline.getHours())}:${String(deadline.getMinutes())}`;
+                bot.telegram.sendMessage(
+                    telegramId,
                     `You have the following deadline expire in <b>6 hours</b>:\n\n` +
                     `<b>Course Name</b>: ${assignment.course}\n\n` +
                     `<b>Assignment Name</b>: ${assignment.assignment}\n\n` +
+                    `<b>Deadline</b>: ${date}\n\n` +
                     `<b>Hurry up!</b>`,
                     {
                         parse_mode: "HTML"
@@ -26,7 +31,7 @@ async function scheduleNotifications(assignments, resources, ctx) {
                 );
 
             };
-                
+
         }, delay);
     };
 };
